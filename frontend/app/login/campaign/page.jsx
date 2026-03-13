89fa99fb68ca1f1f5d0af35cd3c7f12823a0d6ce
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-03-13 14:12:14 +0530

    First commit

---
+++ frontend/app/login/campaign/page.jsx
@@ -0,0 +1,13 @@
+import Container from "@/components/Container";
+
+const LoginPage = () => {
+  return (
+    <section>
+      <div>
+        <Container>Login page</Container>
+      </div>
+    </section>
+  );
+};
+
+export default LoginPage;
---
+++ frontend/app/login/simulation/[id]/page.jsx
@@ -0,0 +1,13 @@
+import Container from "@/components/Container";
+
+const SimulationPage = () => {
+  return (
+    <section>
+      <div>
+        <Container>simulation page</Container>
+      </div>
+    </section>
+  );
+};
+
+export default SimulationPage;
